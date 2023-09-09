a570d03b7f9be9aa2f63a3ca33be3d3ec9bae210
Ahmad Usman <ahmadujuwon@gmail.com>
2023-09-09 04:44:07 +0100

    task 1 initial commit

---
+++ main.go
@@ -0,0 +1,49 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+	"time"
+
+	"github.com/gin-gonic/gin"
+)
+
+type submission_details struct {
+	SlackName     string `json:"slack_name"`
+	CurrentDay    string `json:"current_day"`
+	UTCTime       string `json:"utc_time"`
+	Track         string `json:"track"`
+	GithubFileUrl string `json:"github_file_uri"`
+	GithubRepoUrl string `json:"github_repo_url"`
+	StatusCode    int    `json:"status_code"`
+}
+
+func getSubmissionDetails(c *gin.Context) {
+	slack_name := c.Query("slack_name")
+	track := c.Query("track")
+
+	fmt.Println(time.Now().UTC())
+	fmt.Println(time.Now().Weekday())
+
+	curr_utc_time := time.Now().UTC()
+
+	s_d := submission_details{
+		SlackName:     slack_name,
+		Track:         track,
+		UTCTime:       curr_utc_time.Format("2006-01-02T15:04:05Z"),
+		CurrentDay:    curr_utc_time.Weekday().String(),
+		GithubFileUrl: "random",
+		GithubRepoUrl: "rannnnnn",
+		StatusCode:    200,
+	}
+
+	c.JSON(http.StatusOK, s_d)
+}
+
+func main() {
+	router := gin.Default()
+
+	router.GET("/submission-details", getSubmissionDetails)
+
+	router.Run("localhost:8080")
+}
